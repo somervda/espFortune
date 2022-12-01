c741b2507af71c3a081fe7bf8170cb0c7ae3bc23
David SurfacePro <somervda@comcast.net>
2022-11-30 17:38:20 -0800

    Rerun, Lots of fortunenn.py files made

---
+++ fortuneMaker/fortune13.py
@@ -0,0 +1,3 @@
+F1={'L1' : ' Do what you','L2' : 'know and','L3' : 'perception is','L4' : 'converted into','L5' : 'character.'}
+F2={'L1' : ' Action is more','L2' : 'generally','L3' : 'understood than','L4' : 'words.','L5' : ''}
+F3={'L1' : ' Never give','L2' : 'advice in a','L3' : 'crowd.','L4' : '','L5' : ''}
